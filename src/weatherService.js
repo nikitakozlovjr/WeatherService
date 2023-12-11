048f8c1f49fafdbd6e0b004110272b084f2fe0c2
nikitakozlovjr <nikita3726898@gmail.com>
2023-12-11 23:46:48 +0300

    Прописали apiUrl, добавили apiKey и дописали get запрос

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,14 +1,18 @@
-const apiUrl = '';
+import fs from 'node:fs';
+
+const apiUrl = 'https://api.weather.yandex.ru/v2/informers?';
+const apiKey = fs.readFileSync('__fixtures__/api_key_weather.txt', 'utf-8');
 
 class WeatherService {
     constructor(httpClient) {
         this.httpClient = httpClient;
     };
 
-    async request(city) {
-        const url = new URL(`cities/${city}`, apiUrl);
-        const response = await this.httpClient.get(url);
-        return response;
+    async request([lat, lon]) {
+        const url = new URL(`lat=${lat}&lon=${lon}`, apiUrl);
+        const response = await this.httpClient.get(`${url}\nX-Yandex-API-Key: ${apiKey}`);
+        const data = JSON.parse(response.data);
+        return data;
     }
 };
 
