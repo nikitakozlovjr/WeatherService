e715eb8d244cd311d8fe897dbad9b454135dd8df
nikitakozlovjr <nikita3726898@gmail.com>
2023-12-14 13:15:52 +0300

    Написали вывод пользователю

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,6 +1,9 @@
 const getWeather = (weatherService, coordinates) => {
     return weatherService.request(coordinates).then((response) => {
-        console.log(response);
+        const temp = response.fact.temp; // temperature on thermometer
+        const feelsLike = response.fact.feels_like; // how does it feel
+
+        console.log(`Now the temperature at these coordinates is ${temp} degrees, but feels like ${feelsLike}`);
     })
 };
 
