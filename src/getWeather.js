e24cf3c5d6addbabef65fd8c1e5e3129275461cd
nikitakozlovjr <nikita3726898@gmail.com>
2023-12-14 12:36:40 +0300

    Убрали парсинг

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,7 +1,6 @@
 const getWeather = (weatherService, coordinates) => {
     return weatherService.request(coordinates).then((response) => {
-        const data = JSON.parse(response.data);
-        return data;
+        console.log(response);
     })
 };
 
